aff415d1f91a2bd1a71b7a1641460856f59be60f
nimmitahsin1727@gmail.com <nimmitahsin1727@gmail.com>
2023-12-11 22:39:52 -0500

    feat: tfidf_train_extra_stopwords_admagd_model

--- model/admagd.py
+++ model/admagd.py
@@ -1,28 +1,49 @@
 import numpy as np
 from gensim.models import AuthorTopicModel
 
+
 class AuthorTopicModel:
-    def __init__(self, corpus=None, num_topics=10, id2word=None, author2doc=None, *args, **kwargs):
+    def __init__(
+        self, corpus=None, num_topics=10, id2word=None, author2doc=None, *args, **kwargs
+    ):
         self.corpus = corpus
         self.num_topics = num_topics
         self.id2word = id2word
         self.author2doc = author2doc
 
+
 class ADMAGD(AuthorTopicModel):
-    def __init__(self, corpus=None, num_topics=10, id2word=None, authors=None, 
-                 alpha_init=0.1, beta_init=0.1, a_init=0.1, b_init=0.1, *args, **kwargs):
-        
+    def __init__(
+        self,
+        corpus=None,
+        num_topics=10,
+        id2word=None,
+        authors=None,
+        alpha_init=0.1,
+        beta_init=0.1,
+        a_init=0.1,
+        b_init=0.1,
+        *args,
+        **kwargs,
+    ):
         self.vocab_size = len(id2word)
-        
+
         self.alpha = np.full(num_topics, alpha_init)
         self.beta = np.full(len(id2word), beta_init)
         self.a = a_init
         self.b = b_init
-        
+
         self.authors = list(authors.keys())
-        
-        super(ADMAGD, self).__init__(corpus=corpus, num_topics=num_topics, id2word=id2word, author2doc=authors, *args, **kwargs)
-        
+
+        super(ADMAGD, self).__init__(
+            corpus=corpus,
+            num_topics=num_topics,
+            id2word=id2word,
+            author2doc=authors,
+            *args,
+            **kwargs,
+        )
+
         self.word_topic_matrix = np.zeros((num_topics, len(self.id2word)))
         self.author_topic_matrix = np.zeros((len(self.authors), num_topics))
         self.topic_counts = np.zeros(num_topics)
@@ -41,7 +62,9 @@ class ADMAGD(AuthorTopicModel):
             self.topic_assignments.append(current_doc_assignments)
 
     def get_author(self, doc_id):
-        return next(author for author, docs in self.author2doc.items() if doc_id in docs)
+        return next(
+            author for author, docs in self.author2doc.items() if doc_id in docs
+        )
 
     def get_current_topic(self, doc_id, word_pos):
         return self.topic_assignments[doc_id][word_pos]
@@ -54,37 +77,93 @@ class ADMAGD(AuthorTopicModel):
         self.author_topic_matrix[author_idx][current_topic] -= 1
 
     def calculate_phi_update(self):
-        return (self.word_topic_matrix + self.beta) / (self.word_topic_matrix.sum(axis=1)[:, np.newaxis] + self.vocab_size * self.beta)
+        return (self.word_topic_matrix + self.beta) / (
+            self.word_topic_matrix.sum(axis=1)[:, np.newaxis]
+            + self.vocab_size * self.beta
+        )
 
     def calculate_theta_update(self):
-        return (self.author_topic_matrix + self.alpha) / (self.author_topic_matrix.sum(axis=1)[:, np.newaxis] + self.num_topics * self.alpha)
+        return (self.author_topic_matrix + self.alpha) / (
+            self.author_topic_matrix.sum(axis=1)[:, np.newaxis]
+            + self.num_topics * self.alpha
+        )
 
     def calculate_topic_probabilities(self, doc_id, word_id, author):
         author_idx = self.authors.index(author)
         author_topic_totals = self.author_topic_matrix[author_idx]
-        author_probs = author_topic_totals / sum(author_topic_totals) if sum(author_topic_totals) != 0 else np.ones(self.num_topics) / self.num_topics
+        author_probs = (
+            author_topic_totals / sum(author_topic_totals)
+            if sum(author_topic_totals) != 0
+            else np.ones(self.num_topics) / self.num_topics
+        )
         word_probs = self.calculate_phi_update()[:, word_id]
         combined_probs = author_probs * word_probs
-        normalized_probs = combined_probs / combined_probs.sum()
+
+        if np.any(combined_probs < 0):
+            raise ValueError(f"Negative probabilities detected: {combined_probs}")
+
+        # Normalization step
+        normalized_probs = combined_probs / np.sum(combined_probs)
+
+        if np.any(normalized_probs < 0):
+            raise ValueError(
+                f"Normalization resulted in negative probabilities: {normalized_probs}"
+            )
+
+        # Normalization check
+        if not np.isclose(np.sum(normalized_probs), 1):
+            raise ValueError(f"Probabilities do not sum to 1: {normalized_probs}")
+
         return normalized_probs
 
     def gibbs_sampling(self, iterations=10):
         for iteration in range(iterations):
-            print(f"iteration: {iteration}")
+            print(f"iteration: {iteration+1}")
             for doc_id, document in enumerate(self.corpus):
+                author = self.get_author(doc_id)
+
+                # Pre-compute some values that are reused
+                author_idx = self.authors.index(author)
+
                 for word_pos, (word_id, _) in enumerate(document):
                     current_topic = self.get_current_topic(doc_id, word_pos)
-                    author = self.get_author(doc_id)
                     self.decrement_counts(doc_id, word_pos, current_topic, author)
-                    topic_probs = self.calculate_topic_probabilities(doc_id, word_id, author)
+
+                    # Calculate topic probabilities
+                    topic_probs = self.calculate_topic_probabilities(
+                        doc_id, word_id, author
+                    )
                     if any(val < 0 for val in topic_probs):
                         print("Negative probabilities detected:", topic_probs)
-        #Handle or fix the negative probabilities here
 
-                    topic_probs = topic_probs / sum(topic_probs)
+                    topic_probs = topic_probs / np.sum(topic_probs)
                     new_topic = np.random.choice(self.num_topics, p=topic_probs)
+
                     self.topic_assignments[doc_id][word_pos] = new_topic
-                    self.word_topic_matrix[new_topic][word_id] += 1
+
+                    # Update matrices using vectorized operations if possible
+                    self.word_topic_matrix[new_topic, word_id] += 1
                     self.topic_counts[new_topic] += 1
-                    author_idx = self.authors.index(author)
-                    self.author_topic_matrix[author_idx][new_topic] += 1
+                    self.author_topic_matrix[author_idx, new_topic] += 1
+
+    # def gibbs_sampling(self, iterations=10):
+    #     for iteration in range(iterations):
+    #         print(f"iteration: {iteration+1}")
+    #         for doc_id, document in enumerate(self.corpus):
+    #             for word_pos, (word_id, _) in enumerate(document):
+    #                 current_topic = self.get_current_topic(doc_id, word_pos)
+    #                 author = self.get_author(doc_id)
+    #                 self.decrement_counts(doc_id, word_pos, current_topic, author)
+    #                 topic_probs = self.calculate_topic_probabilities(
+    #                     doc_id, word_id, author
+    #                 )
+    #                 if any(val < 0 for val in topic_probs):
+    #                     print("Negative probabilities detected:", topic_probs)
+
+    #                 topic_probs = topic_probs / sum(topic_probs)
+    #                 new_topic = np.random.choice(self.num_topics, p=topic_probs)
+    #                 self.topic_assignments[doc_id][word_pos] = new_topic
+    #                 self.word_topic_matrix[new_topic][word_id] += 1
+    #                 self.topic_counts[new_topic] += 1
+    #                 author_idx = self.authors.index(author)
+    #                 self.author_topic_matrix[author_idx][new_topic] += 1
